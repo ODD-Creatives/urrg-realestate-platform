beb18f64ea2a649f8ad23248f077bcff21e66f1b
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-07-23 10:13:11 +0100

    comment

--- .history/resources/views/home.blade_20250705045919.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    @include('frontend.home.hero')
-    @include('frontend.home.about')
-    @include('frontend.home.whoweserve')
-    
-@endsection 
-  
