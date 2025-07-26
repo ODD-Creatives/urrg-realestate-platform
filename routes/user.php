b1f046e468acdcf8bdd879527f741ba6663fd51e
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-07-26 10:15:47 +0100

    comment

--- routes/user.php
+++ routes/user.php
@@ -3,15 +3,16 @@
 use App\Http\Controllers\User\ProfileController;
 use App\Http\Controllers\User\UserDashboardController;
 use Illuminate\Support\Facades\Route;
+  
  
-
 
 Route::middleware(['auth'])->group(function () {
     Route::get('user/profile', [ProfileController::class, 'index'])->name('user.profile');
 
     Route::get('user/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update'); 
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('/profile/personal', [ProfileController::class, 'updatePersonal'])->name('update.personal');
    
     Route::get('/user/dashboard', [UserDashboardController::class, 'index'])->name('user.dashboard');
     Route::get('/user/referral', [UserDashboardController::class, 'referral'])->name('user.referral');
