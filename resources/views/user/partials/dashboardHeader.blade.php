de914204b80809f4f0f848940d808f0b349a6ad3
Eshanokpe Daniel <eshanokpe@gmail.com>
2025-07-26 10:00:41 +0100

    comment

--- resources/views/user/partials/dashboardHeader.blade.php
+++ resources/views/user/partials/dashboardHeader.blade.php
@@ -39,18 +39,18 @@
                         <div class="col-4">
                             <h5>Level 1</h5>
                             <p class="mb-1">₦{{ number_format($commissionBreakdown['level1']['amount']) }}</p>
-                            <small>{{ $commissionBreakdown['level1']['count'] }} referrals</small>
+                            <small>{{ $commissionBreakdown['level1']['count'] }} </small>
                         </div>
                         <div class="col-4">
                             <h5>Level 2</h5>
                             <p class="mb-1">₦{{ number_format($commissionBreakdown['level2']['amount']) }}</p>
-                            <small>{{ $commissionBreakdown['level2']['count'] }} referrals</small>
+                            <small>{{ $commissionBreakdown['level2']['count'] }} </small>
                         </div>
-                        <div class="col-4">
+                        {{-- <div class="col-4">
                             <h5>Level 3</h5>
                             <p class="mb-1">₦{{ number_format($commissionBreakdown['level3']['amount']) }}</p>
                             <small>{{ $commissionBreakdown['level3']['count'] }} referrals</small>
-                        </div>
+                        </div> --}}
                     </div>
                 </div>
             </div>
